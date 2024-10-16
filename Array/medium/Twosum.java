57452d18270124fe688dac78e84244e7d07171ae
saurabh nagayach <113882704+saurabhnagaich0817@users.noreply.github.com>
2024-10-16 13:14:08 +0530

    Create Twosum.java

---
+++ Array/medium/Twosum.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static int[] twoSum(int n, int []arr, int target) {
+        int[] ans = new int[2];
+        ans[0] = ans[1] = -1;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] + arr[j] == target) {
+                    ans[0] = i;
+                    ans[1] = j;
+                    return ans;
+                }
+            }
+        }
+        return ans;
+    }
